4511e275618f38719c5095eea313c36ef7526f4c
Udo Schroeter <udo.schroeter@gmail.com>
2017-09-19 16:23:25 +0200

    priority queue

--- u-js-priorityqueue/README.md
+++ u-js-priorityqueue/README.md
@@ -1,1 +1,31 @@
 # Udolib Priority Queue
+
+## Creating a Queue
+
+```
+  var p = PriorityQueue.create();
+```
+
+## Pushing Items onto the Queue
+
+```
+  p.push(1);
+  p.push(11);
+  p.push(2);
+  p.push(9);
+  p.push(5);
+```
+
+## Popping Items from the Queue
+
+```
+  while(pq.peek()) {
+    console.log(pq.pop());
+  }
+```
+
+## Trimming the Queue's Internal Storage
+
+```
+  p.cleanup();
+```
---
+++ u-js-priorityqueue/u-priorityqueue.js
@@ -0,0 +1,92 @@
+/**
+ * Udolib JavaScript Priority Queue
+ * (c) Udo Schroeter udo@openfu.com
+ * License: Public Domain
+ * losely based on https://github.com/lemire/FastPriorityQueue.js
+*/
+
+var PriorityQueue = {
+
+  defaultCompare : function(a, b) {
+    return( a < b );
+  },
+
+  create : function(optionalCustomCompareFunction) {
+
+    var q = {
+      compare : optionalCustomCompareFunction || PriorityQueue.defaultCompare,
+      items : [],
+      size : 0,
+    };
+
+    q.push = function(v) {
+      var i = q.size;
+      q.items[q.size] = v;
+      q.size += 1;
+      var p;
+      var ap;
+      while (i > 0) {
+          p = (i - 1) >> 1;
+          ap = q.items[p];
+          if (!q.compare(v, ap)) {
+            break;
+          }
+          q.items[i] = ap;
+          i = p;
+      }
+      q.items[i] = v;      
+    }
+
+    q.percolateDown = function(i) {
+      var size = q.size;
+      var hsize = q.size >>> 1;
+      var ai = q.items[i];
+      var l;
+      var r;
+      var bestc;
+      while (i < hsize) {
+        l = (i << 1) + 1;
+        r = l + 1;
+        bestc = q.items[l];
+        if (r < size) {
+            if (q.compare(q.items[r], bestc)) {
+                l = r;
+                bestc = q.items[r];
+            }
+        }
+        if (!q.compare(bestc, ai)) {
+            break;
+        }
+        q.items[i] = bestc;
+        i = l;
+      }
+      q.items[i] = ai;      
+    }
+
+    q.peek = function() {
+      if(q.size == 0) 
+        return(false);
+      return(q.items[0]);
+    }
+
+    q.pop = function() {
+      if(q.size == 0) 
+        return(false);
+      var res = q.items[0];
+      if (q.size > 1) {
+        q.items[0] = q.items[--q.size];
+        q.percolateDown(0 | 0);
+      } else {
+        q.size -= 1;
+      }
+      return(res);
+    }
+
+    q.cleanup = function() {
+      q.items = q.items.slice(0, q.size);
+    }
+
+    return(q);
+  },
+
+}
