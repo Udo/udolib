16aabba9c8fa91a6c5cc56063c11126c43d2d043
Udo Schroeter <udo@kautschuk.com>
2021-03-02 16:42:54 +0100

    working on graphedit

--- graphedit-js/graphedit.js
+++ graphedit-js/graphedit.js
@@ -1,32 +1,29 @@
 var id_counter = 1;
 
-function GraphEdit(container, opt) {
+function GraphEdit(container, options) {
 
     id_counter++;
 
     this.container = container;
-    this.opt = opt;
+    this.data = {
+	    options : options,
+	    boxes : {},
+    };
     this.id = 'graphedit-' + id_counter;
-    var elements = this.elements = [];
 
-    if (!opt.width) opt.width = (container.style.width + '');
-    if (!opt.height) opt.height = (container.style.height + '');
-    if (!opt.highlight_color) opt.highlight_color = 'yellow';
-    if (!opt.box_color) opt.box_color = 'rgba(0,0,0,0.1)';
-    if (!opt.box_border_color) opt.box_border_color = 'rgba(0,0,0,0.2)';
-    if (!opt.box_opts) opt.box_opts = 'rx="12" ry="12"';
-    if (!opt.box_width) opt.box_width = (opt.grid ? opt.grid * 8 : 280);
-    if (!opt.box_height) opt.box_height = (opt.grid ? opt.grid * 5 : 160);
-    if (!opt.connector_color) opt.connector_color = 'green';
-    if (!opt.connector_border_color) opt.connector_border_color = 'white';
-    opt.default_pos = 0;
+    if (!options.width) options.width = (container.style.width + '');
+    if (!options.height) options.height = (container.style.height + '');
+    if (!options.highlight_color) options.highlight_color = 'yellow';
+    if (!options.box_color) options.box_color = 'rgba(0,0,0,0.1)';
+    if (!options.box_border_color) options.box_border_color = 'rgba(0,0,0,0.2)';
+    if (!options.opts) options.opts = 'rx="12" ry="12"';
+    if (!options.box_width) options.box_width = (options.grid ? options.grid * 8 : 280);
+    if (!options.box_height) options.box_height = (options.grid ? options.grid * 5 : 160);
+    if (!options.connector_color) options.connector_color = 'green';
+    if (!options.connector_border_color) options.connector_border_color = 'white';
+    options.default_pos = 0;
 
-    this.container.innerHTML = '<svg id="' + this.id + '" viewBox="0 0 ' +
-        opt.width.substr(0, opt.width.length - 2) + ' ' +
-        opt.height.substr(0, opt.height.length - 2) +
-        '" xmlns="http://www.w3.org/2000/svg"></svg>';
-
-    var stage = this.stage = document.getElementById(this.id);
+    var stage = false;
     var mouse = this.mouse = {
         button: false,
         x: 0,
@@ -35,131 +32,65 @@ function GraphEdit(container, opt) {
         drag_y: 0,
         selected_object: false,
     }
-
-    var add_rect = this.add_rect = function(box_opt) {
-        var box_id = 'e-' + (++id_counter);
-        stage.innerHTML += '<rect id="' + box_id + '" ' +
-            'style="fill:' + (box_opt.color || opt.box_color) + ';stroke-width:0;stroke:' +
-            (box_opt.border_color || opt.box_border_color) + ';" ' +
-            'x="' + (box_opt.x) + '" y="' + (box_opt.y) +
-            '" width="' + (box_opt.width || opt.width) + '" height="' +
-            (box_opt.height || opt.box_height) + '" ' + (box_opt.opts || '') + ' />';
-        return (box_id);
-    }
-
-    var add_connector = this.add_connector = function(box_id, con_opt) {
-        var connector_id = 'c-' + (++id_counter);
-        con_opt.id = connector_id;
-		con_opt.type = 'connector';
-		elements.push(con_opt);
-
-        var box = document.getElementById(box_id);
-        if (!box.innerHTML) box.innerHTML = '';
-        box.innerHTML += '<circle cx="50" cy="50" x="50" y="50" r="10" ' +
+    
+    var render_connectors = (opt) => {
+	    var r = '';
+	    opt.connectors.forEach((con, idx) => {
+		    r += '<circle cx="50" cy="50" r="10" transform="translate(' + (con.x) + ',' + (con.y) + ')" ' +
             ' pointer-events="visiblePoint" ' +
             ' is-connectable="true"' +
-            ' style="stroke:' + (con_opt.border_color || opt.connector_border_color) +
-            ';stroke-width:1;fill:' + (con_opt.border_color || opt.connector_border_color) + '" />';
-    }
-
-    var add_box = this.add_box = function(box_opt) {
-        var box_id = 'e-' + (++id_counter);
-        box_opt.id = box_id;
-        box_opt.type = 'box';
-        elements.push(box_opt);
-        if (typeof box_opt.x == 'undefined') {
-            opt.default_pos += (opt.grid ? opt.grid * 4 : 100);
-            box_opt.x = box_opt.y = opt.default_pos;
-            console.log(opt.default_pos, box_id);
-        }
-        stage.innerHTML += ('<g id="' + box_id + '" x="' + (box_opt.x) + '" y="' + (box_opt.y) + '" transform="translate(' + (box_opt.x) + ',' + (box_opt.y) + ')">' +
-            '<rect id="b-' + box_id + '" is-draggable="true" drag="' + box_id + '" pointer-events="visiblePoint" ' +
-            'style="fill:' + (box_opt.color || opt.box_color) + ';stroke-width:1;stroke:' +
-            (box_opt.border_color || opt.box_border_color) + ';" ' +
-            ' width="' + (box_opt.width || opt.box_width) + '" height="' +
-            (box_opt.height || opt.box_height) + '" ' + (box_opt.opts || opt.box_opts) + ' /></g>');
-        add_connector(box_id, {});
-        return (box_id);
-    }
-
-    var make_connection_path = this.make_connection_path = function(x1, y1, x2, y2) {
-        var mx1 = (x2 - x1) * 0.5;
-        return ('M' + x1 + ' ' + y1 + ' ' +
-            ' C' + (x1 + mx1) + ' ' + (y1) + ' ' +
-            ' ' + (x1 + mx1) + ' ' + (y2) + ' ' +
-            ' ' + x2 + ' ' + y2 + ' ');
-    }
-
-    var add_connection = this.add_connection = function(opt) {
-        var eid = 'e-' + (++id_counter);
-        opt.type = 'connection';
-        opt.id = eid;
-		elements.push(opt);
-        stage.innerHTML += '<path id="' + eid + '" d="' + make_connection_path(opt.x1, opt.y1, opt.x2, opt.y2) + '" style="fill:none;stroke:blue;stroke-width:3;"/>';
-        return (eid);
+            ' style="stroke:' + (con.border_color || options.connector_border_color) +
+            ';stroke-width:1; fill:' + (con.border_color || options.connector_border_color) + '" />';
+	    });
+	    return(r);
     }
     
-    var remove_connection = this.remove_connection = function(eid) {
-		var element = document.getElementById(eid);
-		element.parentNode.removeChild(element);
-	}
-
-    var highlight_element = this.highlight_element = function(e) {
-        var hl = e.getAttribute('hl');
-        if (!hl) hl = opt.highlight_color;
-        if (!e.getAttribute('og-color')) e.setAttribute('og-color', e.style.stroke);
-        if (!e.getAttribute('og-stroke-width')) e.setAttribute('og-stroke-width', e.style['stroke-width']);
-        e.style.stroke = hl;
-        e.style['stroke-width'] = 4;
+    var render_box = (opt) => {
+	    return('<g id="' + opt.id + '" x="' + (opt.x) + 
+        	'" y="' + (opt.y) + '" transform="translate(' + (opt.x) + ',' + (opt.y) + ')">' +
+            '<rect id="b-' + opt.id + '" is-draggable="true" drag="' + opt.id + 
+            '" pointer-events="visiblePoint" ' +
+            'style="fill:' + (opt.color || options.box_color) + ';stroke-width:1;stroke:' +
+            (opt.border_color || options.box_border_color) + ';" ' +
+            ' width="' + (opt.width || options.box_width) + '" height="' +
+            (opt.height || options.box_height) + '" />'+render_connectors(opt)+'</g>');
     }
-
-    var unhighlight_element = this.unhighlight_element = function(e) {
-        if (e.getAttribute('og-color')) {
-            e.style.stroke = e.getAttribute('og-color');
-            e.style['stroke-width'] = e.getAttribute('og-stroke-width');
-        }
+    
+    var render_boxes = () => {
+		var r = '';
+		for (var key in this.data.boxes) if (this.data.boxes.hasOwnProperty(key)) {
+			r += render_box(this.data.boxes[key]);	
+		}
+		return(r);
     }
-
-    var grid_snap = this.grid_snap = function(c0) {
-        if (!opt.grid) return (c0);
-        return (Math.round(c0 / opt.grid) * opt.grid);
+    
+	var render_rect = function(opt) {
+        var box_id = 'e-' + (++id_counter);
+        return('<rect id="' + box_id + '" ' +
+            'style="fill:' + (opt.color || options.box_color) + ';stroke-width:0;stroke:' +
+            (opt.border_color || options.box_border_color) + ';" ' +
+            'x="' + (opt.x) + '" y="' + (opt.y) +
+            '" width="' + (opt.width || options.width) + '" height="' +
+            (opt.height || options.box_height) + '" />');
     }
-
-    var get_element_pos = this.get_element_pos = function(e) {
-        var parent = e.parentNode;
-        var result = {
-            x: e.getAttribute('x') * 1.0,
-            y: e.getAttribute('y') * 1.0
-        };
-        if (parent && parent.tagName == 'g') {
-            result.x += parent.getAttribute('x') * 1.0;
-            result.y += parent.getAttribute('y') * 1.0;
-        }
-        console.log(result);
-        return (result);
+    
+    var render_grid = () => {
+	    return('<defs><pattern id="grid" width="' +
+            options.grid + '" height="' + options.grid + '" patternUnits="userSpaceOnUse">' +
+            '<path d="M ' + options.grid + ' 0 L 0 0 0 ' + options.grid +
+             '" style="stroke:rgb(0,0,0,0.1);fill:none;stroke-width:1;"/>' +
+            '</pattern></defs>'+render_rect({
+	            color: 'url(#grid)',
+	            x: 0,
+	            y: 0,
+	            width: '100%',
+	            height: '100%'
+	    }));
     }
+    
+    this.on = {
 
-    var clear = this.clear = function() {
-		
-        stage.innerHTML = '<defs><pattern id="grid" width="' +
-            opt.grid + '" height="' + opt.grid + '" patternUnits="userSpaceOnUse">' +
-            '<path d="M ' + opt.grid + ' 0 L 0 0 0 ' + opt.grid + '" style="stroke:rgb(0,0,0,0.1);fill:none;stroke-width:1;"/>' +
-            '</pattern></defs>';
-
-        add_rect({
-            color: 'url(#grid)',
-            x: 0,
-            y: 0,
-            width: '100%',
-            height: '100%'
-        });
-    }
-
-    clear();
-
-    var default_handler = this.default_handler = {
-
-        mousemove: (e) => {
+        mousemove : (e) => {
             mouse.x = e.clientX;
             mouse.y = e.clientY;
             if (mouse.button && mouse.is_dragging) {
@@ -173,11 +104,12 @@ function GraphEdit(container, opt) {
                     mouse.drag.setAttribute('transform', 'translate(' + tx + ',' + ty + ')');
                 }
             } else if (mouse.button && mouse.is_connecting) {
-                mouse.path_element.setAttribute('d', make_connection_path(mouse.drag_x, mouse.drag_y, mouse.x, mouse.y));
+                mouse.path_element.setAttribute('d', 
+                	make_connection_path(mouse.drag_x, mouse.drag_y, mouse.x, mouse.y));
             }
         },
 
-        mousedown: (e) => {
+        mousedown : (e) => {
             if (e.button == 0) mouse.button = true;
             mouse.drag_x = mouse.x;
             mouse.drag_y = mouse.y;
@@ -207,9 +139,9 @@ function GraphEdit(container, opt) {
                 mouse.is_connecting = false;
             }
             // console.log(e.target.tagName, mouse);
-        },
+        }, 
 
-        mouseup: (e) => {
+        mouseup : (e) => {
             if (e.button == 0) mouse.button = false;
             console.log('mouseup', e.target.tagName, e.target.id, mouse);
             if (mouse.is_dragging) {
@@ -230,26 +162,95 @@ function GraphEdit(container, opt) {
 
     }
 
-    var on = this.on = {
+	this.render = () => {
+		var content = '<svg id="' + this.id + '" viewBox="0 0 ' +
+	        options.width.substr(0, options.width.length - 2) + ' ' +
+	        options.height.substr(0, options.height.length - 2) +
+	        '" xmlns="http://www.w3.org/2000/svg">'+
+	        	render_grid()+
+	        	render_boxes()+'</svg>';
+	    console.log(content);
+	    this.container.innerHTML = content;
+	    stage = document.getElementById(this.id);
 
-        mousemove: default_handler.mousemove,
-        mousedown: default_handler.mousedown,
-        mouseup: default_handler.mouseup,
+	    stage.addEventListener('mousemove', this.on.mousemove);
+	    stage.addEventListener('mousedown', this.on.mousedown);
+	    stage.addEventListener('mouseup', this.on.mouseup);	
+	} 
 
+    var add_box = this.add_box = (opt) => {
+        opt.id ='e-' + (++id_counter);
+        opt.type = 'box';
+        if (typeof opt.x == 'undefined') {
+            options.default_pos += (options.grid ? options.grid * 4 : 100);
+            opt.x = opt.y = options.default_pos;
+        }
+        opt.connectors = [];
+        opt.connectors.push({ type : 'in', x : 1, y : 1 });
+        this.data.boxes[opt.id] = opt;
+		this.render();
     }
 
+    var make_connection_path = this.make_connection_path = (x1, y1, x2, y2) => {
+        var mx1 = (x2 - x1) * 0.5;
+        return ('M' + x1 + ' ' + y1 + ' ' +
+            ' C' + (x1 + mx1) + ' ' + (y1) + ' ' +
+            ' ' + (x1 + mx1) + ' ' + (y2) + ' ' +
+            ' ' + x2 + ' ' + y2 + ' ');
+    }
 
-    stage.addEventListener('mousemove', (e) => {
-        if (on.mousemove) return (on.mousemove(e));
-    });
+    var add_connection = this.add_connection = (opt) => {
+        var eid = 'e-' + (++id_counter);
+        opt.type = 'connection';
+        opt.id = eid;
+		elements.push(opt);
+        stage.innerHTML += '<path id="' + eid + '" d="' + 
+        	make_connection_path(opt.x1, opt.y1, opt.x2, opt.y2) + 
+        	'" style="fill:none;stroke:blue;stroke-width:3;"/>';
+        return (eid);
+    }
+    
+    var remove_connection = this.remove_connection = (eid) => {
+		var element = document.getElementById(eid);
+		element.parentNode.removeChild(element);
+	}
 
-    stage.addEventListener('mousedown', (e) => {
-        if (on.mousedown) return (on.mousedown(e));
-    });
+    var highlight_element = this.highlight_element = (e) => {
+        var hl = e.getAttribute('hl');
+        if (!hl) hl = opt.highlight_color;
+        if (!e.getAttribute('og-color')) e.setAttribute('og-color', e.style.stroke);
+        if (!e.getAttribute('og-stroke-width')) e.setAttribute('og-stroke-width', e.style['stroke-width']);
+        e.style.stroke = hl;
+        e.style['stroke-width'] = 4;
+    }
 
-    stage.addEventListener('mouseup', (e) => {
-        if (on.mouseup) return (on.mouseup(e));
-    });
+    var unhighlight_element = this.unhighlight_element = (e) => {
+        if (e.getAttribute('og-color')) {
+            e.style.stroke = e.getAttribute('og-color');
+            e.style['stroke-width'] = e.getAttribute('og-stroke-width');
+        }
+    }
+
+    var grid_snap = this.grid_snap = (c0) => {
+        if (!opt.grid) return (c0);
+        return (Math.round(c0 / opt.grid) * opt.grid);
+    }
+
+    var get_element_pos = this.get_element_pos = (e) => {
+        var parent = e.parentNode;
+        var result = {
+            x: e.getAttribute('x') * 1.0,
+            y: e.getAttribute('y') * 1.0
+        };
+        if (parent && parent.tagName == 'g') {
+            result.x += parent.getAttribute('x') * 1.0;
+            result.y += parent.getAttribute('y') * 1.0;
+        }
+        console.log(result);
+        return (result);
+    }
+
+    this.render();
 
     console.log('init complete');
 
